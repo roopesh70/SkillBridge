45032fb3dbf9df9196a287d641e18b4494a98b73
Roopesh Krishnan <roopeshkrishnanme@gmail.com>
2025-07-25 08:06:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
   	extend: {
       fontFamily: {
-        sans: ["var(--font-inter)", "sans-serif"],
+        sans: ["var(--font-inter)", "system-ui", "sans-serif"],
       },
   		colors: {
   			background: 'hsl(var(--background))',
