afb3bdac12ad6f3ef2108d94f050bb698992f97b
Roopesh Krishnan <roopeshkrishnanme@gmail.com>
2025-07-29 15:06:24 +0000

    what i have to do to safe my api key

--- src/ai/ai-instance.ts
+++ src/ai/ai-instance.ts
@@ -1,12 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebaseConfig} from '@/lib/firebase';
 
 export const ai = genkit({
   promptDir: './prompts',
   plugins: [
     googleAI({
-      apiKey: firebaseConfig.apiKey,
+      apiKey: process.env.GOOGLE_API_KEY,
     }),
   ],
   model: 'googleai/gemini-2.0-flash',
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  projectId: "studio-w1rp4",
-  appId: "1:967303337304:web:632c012dbe7dc43406812d",
-  storageBucket: "studio-w1rp4.appspot.com",
-  apiKey: "AIzaSyCUAjQXelwGmeh0H8EbEpnbymoIHlpgB7M",
-  authDomain: "studio-w1rp4.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "967303337304",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
